9c3d1fbb52d23d4966ff2b4d652f17a6efa0a834
vagrant <vagrant@precise32.(none)>
2013-06-25 14:59:16 +0000

    created rails environment

---
+++ preddit/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Preddit::Application.config.secret_key_base = 'bbdd5f5d048ba5b1a2b870941d870b3531c093393c88ca8ee78257d13d7397b2545f649cf0151f756e1dfd58c974472ab3e13ba6ceb62490e37c32b68c5b2a0a'
---
+++ preddit/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Preddit::Application.config.session_store :cookie_store, key: '_preddit_session'
