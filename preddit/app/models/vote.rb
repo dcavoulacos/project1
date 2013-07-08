8984eab7e561e6292266ea1238b7ce5c844b9610
Dimitri Cavoulacos <d.cavoulacos@gmail.com>
2013-07-08 13:33:57 +0000

    working votes

--- preddit/app/models/vote.rb
+++ preddit/app/models/vote.rb
@@ -3,5 +3,5 @@ class Vote < ActiveRecord::Base
   belongs_to :post
   belongs_to :comment
 
-  validates :user_id, :uniqueness => { :scope => :post_id }
+  validates :user_id, :uniqueness => { :scope => [:post_id, :comment_id] }
 end
--- preddit/app/views/votes/show.json.jbuilder
+++ preddit/app/views/votes/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @vote, :user_id, :post_id, :up, :created_at, :updated_at
+json.extract! @vote, :user_id, :post_id, :comment_id, :up, :created_at, :updated_at
